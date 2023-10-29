d6ecfeb19bcab038984da263eba7c75120b52c7b
Todd Nordquist <toddnordquis144@gmail.com>
2023-10-28 20:36:35 -0600

    Accurately determined win or loss

--- hangman/Program.cs
+++ hangman/Program.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections;
+﻿using System.Data.Common;
+using System.Collections;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Threading.Tasks.Dataflow;
+using Microsoft.VisualBasic;
 
 namespace HangMan
 {
@@ -15,6 +18,8 @@ namespace HangMan
         static private String secretWord = "";
         static int[] secretCharPositions;
         static int guessesLeft = 5;
+        static int wordLength = 0;
+        static bool won = false;
 
         private static void Main(string[] args)
         {
@@ -31,42 +36,56 @@ namespace HangMan
                 Random rng = new(100);
                 int lowerBound = 0;
                 int upperBound = 4;
-                int rInt = rng.Next(lowerBound,upperBound);
+                int rInt = rng.Next(lowerBound, upperBound);
                 secretWord = wordPool[rInt];
                 secretCharPositions = new int[secretWord.Length];
             }
 
             while (guessesLeft > 0)
             {
-                string letterGuessed = (Console.ReadLine());
+                string letterGuessed = (Console.ReadLine()).ToLower();
 
                 if (secretWord.Contains(letterGuessed))
                 {
-                    for(int i = 0; i < secretWord.Length; i ++)
+                    for (int i = 0; i < secretWord.Length; i++)
                     {
-                        if (secretWord[i].ToString().Equals(letterGuessed))
+                        if (secretCharPositions[i] != 1 && secretWord[i].ToString().Equals(letterGuessed))
                         {
                             secretCharPositions[i] = 1;
+                            wordLength++;
+                            Console.WriteLine("You guessed correctly");
+                        }
+                        else if (secretCharPositions[i] == 1 && secretWord[i].ToString().Equals(letterGuessed))
+                        {
+                            Console.WriteLine("You already guessed that letter");
                         }
                     }
-                    Console.WriteLine("Yay");
-                } 
+                }
                 else
                 {
+                    Console.WriteLine("Wrong guess");
                     guessesLeft--;
                 }
-
+                Console.Clear();
+                if (wordLength == secretWord.Length)
+                {
+                    Console.WriteLine("You won: the correct word was " + secretWord);
+                    won = true;
+                }
             }
-
+                if(won == false)
+                {
+                Console.WriteLine("Sorry, you are out of guesses: the correct word was " + secretWord);
+                }
         }
 
         private static void CreateWordPool()
         {
 
-            wordPool.Add("Hello");
-            wordPool.Add("house");
-            wordPool.Add("Sink");
+            wordPool.Add("Hello".ToLower());
             wordPool.Add("catepillar");
+            wordPool.Add("house");
+            wordPool.Add("Sink".ToLower());
             wordPool.Add("hydroponics");
         }
     }
